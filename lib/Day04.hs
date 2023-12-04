4db3cbe6d821a11f1b030bdf17e41771fe29ab5f
lazyguyy <17795787+lazyguyy@users.noreply.github.com>
2023-12-04 18:12:12 +0100

    small modification c:

--- lib/Day04.hs
+++ lib/Day04.hs
@@ -20,10 +20,10 @@ numberParser :: RE Char Int
 numberParser = read <$> many (psym isNumber) <* many (sym ' ')
 
 cardParser :: RE Char Card
-cardParser = makeCard <$> (string "Card" *> many (sym ' ') *> numberParser) <*> (sym ':' *> many (sym ' ') *> many numberParser) <*> (sym '|' *> many (sym ' ') *> many numberParser)
-
-makeCard :: Int -> [Int] -> [Int] -> Card
-makeCard i w a = Card i (Set.fromList w) (Set.fromList a)
+cardParser = Card <$>
+             (string "Card" *> many (sym ' ') *> numberParser) <*>
+             (Set.fromList <$> (sym ':' *> many (sym ' ') *> many numberParser)) <*>
+             (Set.fromList <$> (sym '|' *> many (sym ' ') *> many numberParser))
 
 
 part1 :: [Card] -> String
@@ -35,5 +35,4 @@ part1  = show . sum .  map cardValue
 part2 :: [Card] -> String
 part2 cards = show $ Map.foldr (+) 0 $ foldl (\m -> \c -> updateMap m c) (Map.fromList $ zip [1..(length cards)] $ repeat 1) cards
     where
-        updateMap :: Map.Map Int Int -> Card -> Map.Map Int Int
         updateMap mp (Card i w a) = foldl (\m -> \k -> Map.insertWith (+) k (Map.findWithDefault 1 i mp) m) mp [i + 1..i + (Set.size $ Set.intersection w a)]
